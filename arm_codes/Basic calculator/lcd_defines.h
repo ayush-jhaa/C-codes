db367322bbefeccbddb2e798dc97da0cf6bc886e
Jha-Ayuush <109381535+Jha-Ayuush@users.noreply.github.com>
2023-11-14 10:46:03 +0530

    Create lcd_defines.h

---
+++ arm_codes/Basic calculator/lcd_defines.h
@@ -0,0 +1,32 @@
+#define LCD_D0 8
+#define LCD_D1 9
+#define LCD_D2 10
+#define LCD_D3 11
+#define LCD_D4 12
+#define LCD_D5 13
+#define LCD_D6 14
+#define LCD_D7 15
+#define LCD_RS 16
+#define LCD_RW 17
+#define LCD_EN 18
+
+#define CLEAR_LCD      0X01
+#define CLR_HOME       0X02
+#define SHIFT_DISP_RGT 0X06
+#define SHIFT_DISP_LFT 0X07
+#define DSP_OFF        0X08
+#define DSP_ON_CUR_OFF 0X0C
+#define DSP_ON_CUR_ON  0X0E
+#define DSP_ON_CUR_BLINK 0X0F
+#define MODE_8BIT_1LINE 0X30
+#define MODE_8BIT_2LINE 0X38
+#define MODE_4BIT_1LINE 0X20
+#define MODE_4BIT_2LINE 0X28
+#define GOTO_LINE1_POS0 0X80
+#define GOTO_LINE2_POS0 0XC0
+#define GOTO_LINE3_POS0 0X94
+#define GOTO_LINE4_POS0 0X04
+
+#define GOTO_CGRAM_START 0X40
+#define SHIFT_CUR_LEFT 0X10
+#define SHIFT_CUR_RIGHT 0X14
