9c85ff327dd677c28947352014d23e9b74b9626f
Jha-Ayuush <109381535+Jha-Ayuush@users.noreply.github.com>
2024-01-05 11:40:45 +0530

    Create library_manage_2d_array.c

---
+++ library_manage_2d_array.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main()
+{
+  int x,s,p,b,i=0,cnt=0;
+  static int arr[200][100];
+
+  while(1)
+  {
+     printf("1.enter the shelf and page 2. print the page of book on specific shelf 3. print the books on shelf\n");
+     printf("enter the x value :");
+     scanf("%d",&x);
+
+     if(x==1)
+     {
+         printf("enter the shelf no.");
+         scanf("%d",&s);
+         printf("enter the no. of pages in book");
+         scanf("%d",&p);
+         for(int j=0;j<100;j++)
+         {
+           if(arr[s][j]==0)
+           {
+              arr[s][j]=p;
+              break;
+           }
+         }
+     }
+
+     else if(x==2)
+     {
+          printf("enter the shelf no.");
+          scanf("%d",&s);
+          printf("enter the book no.");
+          scanf("%d",&b);
+
+          if(arr[s][b]==0)
+          {
+             printf("book not present\n");
+
+          }
+          else
+          {
+          printf("shelf %d book no. %d contains: %d pages",s,b,arr[s][b]);
+          printf("\n");
+          }
+     }
+
+     else if(x==3)
+     {
+         printf("enter the shelf  no.");
+         scanf("%d",&s);
+         cnt=0;
+         for(i=0;i<100;i++)
+         {
+            if(arr[s][i] !=0)
+            {
+              cnt++;
+            }
+         }
+
+         if(cnt==0)
+         {
+           printf("book not present there\n");
+         }
+         else
+         {
+         printf("no. of books in a shelf no. %d is : %d\n",s,cnt);
+         }
+
+     }
+
+     else
+        {
+          printf("wrong input\n");
+        }
+  }
+}
